302c131f7a5fed53952daff4d707a65ce708a43b
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-04-12 23:33:30 -0500

    added header, main, footer

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 
+var plusBtn = document.getElement('');
+
+plusBtn.addEventListener('click', function () {
+
+
+    let notesObj;
+	let addNote = document.getElementById('addNote');
+	let notesString = localStorage.getItem('notes');
+	
+	if(notesString == null){
+		notesObj = [];
+	}
+	else{
+		notesObj = JSON.parse(notesString);
+	}
+    
+});
