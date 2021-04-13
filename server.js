32e6feb907f731e9a37113e6e7b1d9dfc181fd7a
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-04-13 02:01:09 -0500

    made server in node

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+const http = require('http');
+const express = require('express');
 
-var plusBtn = document.getElement('');
+const app = express();
 
-plusBtn.addEventListener('click', function () {
+var PORT = process.env.PORT || 3001;
+
+const plusBtn = document.getElementById('plus-btn');
+
+const displayNoteSite = (res) => {
+    fs.readFile(`${__dirname}/index.html`, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+
+    plusBtn.addEventListener('click', function () {
 
 
-    let notesObj;
-	let addNote = document.getElementById('addNote');
-	let notesString = localStorage.getItem('notes');
-	
-	if(notesString == null){
-		notesObj = [];
-	}
-	else{
-		notesObj = JSON.parse(notesString);
-	}
-    
+        let notesObj;
+        let addNote = document.getElementById('addNote');
+        let notesString = localStorage.getItem('notes');
+
+        console.log("clicked note button");
+
+        if (notesString == null) {
+            notesObj = [];
+        }
+        else {
+            notesObj = JSON.parse(notesString);
+        }
+
+    });
+};
+
+const server = http.createServer(handleRequest);
+server.listen(PORT, () => {
+    console.log(`Server is listening on: http://localhost:${PORT}`);
 });
+
